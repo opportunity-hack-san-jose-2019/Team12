2c5f14187cfa7ca405de3be1b7f67c5d64b83051
Yuya Yabe <yuyabee@gmail.com>
2019-11-03 02:48:32 -0800

    Add simple views

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,10 @@
 class HomeController < ApplicationController
   def index
   end
+
+  def interviewer_checkin
+  end
+
+  def interviewee_checkin
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  get "home/index"
   root to: "home#index"
+  get 'landing', action: :index, controller: 'home'
+  get 'interviewer_checkin', action: :interviewer_checkin, controller: 'home'
+  get 'interviewee_checkin', action: :interviewee_checkin, controller: 'home'
 
   devise_for :admins
 end
