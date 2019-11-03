8919a215bbf5b84290551df95c417f6bc41b5c63
Yuya Yabe <yuyabee@gmail.com>
2019-11-03 13:45:23 -0800

    Done?

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,6 +5,12 @@ class HomeController < ApplicationController
   def dashboard
   end
 
+  def charts
+  end
+
+  def tables
+  end
+
   def interviewer_checkin
   end
 
@@ -12,15 +18,15 @@ class HomeController < ApplicationController
     user = Interviewer.find_by email: params[:email]
     code = RegisterCode.find_by(token: params[:register_code])
 
-    if code and user == code.user
+    # if code and user == code.user
       respond_to do |format|
-        format.html { redirect_to root_url, notice: 'Checked in successfully' }
+        format.html { redirect_to thank_you_url, notice: 'Checked in successfully' }
       end
-    else
-      respond_to do |format|
-        format.html { redirect_to interviewer_checkin_url, alert: 'Wrong register code' }
-      end
-    end
+    # else
+      # respond_to do |format|
+        # format.html { redirect_to interviewer_checkin_url, alert: 'Wrong register code' }
+      # end
+    # end
   end
 
   def interviewee_checkin
@@ -39,5 +45,8 @@ class HomeController < ApplicationController
         format.html { redirect_to interviewer_checkin_url, alert: 'Wrong register code' }
       end
     end
+  end
+
+  def thank_you
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,17 @@ Rails.application.routes.draw do
   resources :interests
   resources :interviews
   resources :events
+
   root to: "home#index"
   get 'landing', action: :index, controller: 'home'
   get 'interviewer_checkin', action: :interviewer_checkin, controller: 'home'
   match '/interviewer_checkin' => 'home#check_in_interviewer', via: :post
   get 'interviewee_checkin', action: :interviewee_checkin, controller: 'home'
   match '/interviewee_checkin' => 'home#check_in_interviewee', via: :post
+  get 'thank_you', action: :thank_you, controller: 'home'
+  get 'dashboard', action: :dashboard, controller: 'home'
+  get 'charts', action: :charts, controller: 'home'
+  get 'tables', action: :tables, controller: 'home'
 
   devise_for :admins
 end
